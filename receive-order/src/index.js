b3ff038c0e84dac0410e8a8562a4fee43982e462
renan04lima <renan04lima@outlook.com>
2021-11-06 00:15:15 -0300

    refactor: organize receive-order

--- receive-order/src/index.js
+++ receive-order/src/index.js
@@ -1,36 +1,42 @@
 const express = require('express')
-const app = express()
 const { Kafka } = require('kafkajs')
 
+class OrdersController {
+  constructor() {
+    this.kafka = new Kafka({
+      clientId: 'quickstart_app',
+      brokers: ['localhost:9092']
+    })
+  }
+
+  async handle(req, res) {
+    var order = req.body;
+    order.id = Math.floor(Date.now() / 1000)
+    order.status = 'PENDING_PAYMENT'
+
+    await this.#sendMessage(order)
+
+    return res.json({ order, message: 'Request made successfully. We are reviewing your payment. You will receive information shortly.' })
+  }
+
+  async #sendMessage(order) {
+    const producer = this.kafka.producer()
+    await producer.connect()
+    await producer.send({
+      topic: 'RECEIVE_ORDER',
+      messages: [
+        { value: JSON.stringify(order) },
+      ],
+    })
+    await producer.disconnect()
+  }
+}
+
+const app = express()
 app.use(express.json());
-
+const ordersController = new OrdersController()
+app.post('/orders', ordersController.handle.bind(ordersController))
 const port = 3001
-
-const kafka = new Kafka({
-  clientId: 'quickstart_app',
-  brokers: ['localhost:9092']
-})
-
-app.get('/', (req, res) => {
-  res.send('kafka quickstart')
-})
-
-app.post('/orders', async (req, res) => {
-  var order = req.body;
-  order.id = Math.floor(Date.now() / 1000)
-  order.status = 'PENDING_PAYMENT'
-  const producer = kafka.producer()
-  await producer.connect()
-  await producer.send({
-    topic: 'RECEIVE_ORDER',
-    messages: [
-      { value: JSON.stringify(order) },
-    ],
-  })
-  await producer.disconnect()
-
-  return res.json({ order, message: 'Request made successfully. We are reviewing your payment. You will receive information shortly.' })
-})
 app.listen(port, () => {
   console.log(`Express API http://localhost:${port}`)
 })
