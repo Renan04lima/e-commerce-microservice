4e2188e5fc9432bc266ac86bbae9ddc6c6e590a9
renan04lima <renan04lima@outlook.com>
2021-11-07 17:32:57 -0300

    feat: implement billing microservice

--- billing/src/index.js
+++ billing/src/index.js
@@ -1,5 +1,34 @@
-const run = async () => {
+const { Kafka } = require('kafkajs')
 
+const kafka = new Kafka({
+  clientId: 'quickstart_app',
+  brokers: ['127.0.0.1:9092']
+})
+
+const run = async () => {
+  const consumer = kafka.consumer({ groupId: 'invoice' })
+  await consumer.connect()
+  await consumer.subscribe({ topic: 'PAID_ORDER', fromBeginning: true })
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      var order = JSON.parse(message.value.toString());
+      await billOrder(order);
+    },
+  })
+}
+
+const billOrder = async (order) => {
+  order.nfe = "nfe_" + order.id + "_2020.xml";
+  order.status = "BILLED"
+  const producer = kafka.producer()
+  await producer.connect()
+  await producer.send({
+    topic: 'BILLED_ORDER',
+    messages: [
+      { value: JSON.stringify(order) },
+    ],
+  })
+  await producer.disconnect()
 }
 
 run().catch(console.error)
