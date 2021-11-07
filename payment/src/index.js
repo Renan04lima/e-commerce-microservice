39f4acf62f459be662ae03bed7942e20b12adc3d
renan04lima <renan04lima@outlook.com>
2021-11-07 16:42:05 -0300

    feat: implement payment microservice

--- payment/src/index.js
+++ payment/src/index.js
@@ -1,7 +1,34 @@
+const { Kafka } = require('kafkajs')
+
+const kafka = new Kafka({
+  clientId: 'rocket_app',
+  brokers: ['127.0.0.1:9092']
+})
+
 const run = async () => {
-  console.log(hello);
+  const consumer = kafka.consumer({ groupId: 'pagto' })
+  await consumer.connect()
+  await consumer.subscribe({ topic: 'RECEIVE_ORDER', fromBeginning: true })
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      var order = JSON.parse(message.value.toString());
+      await processPayment(order);
+    },
+  })
+}
+
+const processPayment = async (order) => {
+  order.status = 'PAID';
+  order.payment_id = Math.floor(Date.now() / 1000)
+  const producer = kafka.producer()
+  await producer.connect()
+  await producer.send({
+    topic: 'PAID_ORDER',
+    messages: [
+      { value: JSON.stringify(order) },
+    ],
+  })
+  await producer.disconnect()
 }
 
 run().catch(console.error)
-
-
